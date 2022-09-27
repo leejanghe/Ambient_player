520e7705f7d4c59a94fc917fd62257b555c50a27
leejanghe <babybye777@gmail.com>
2022-09-28 00:33:15 +0900

    마크업 & css & js 작업

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,48 @@
+import "./App.css";
+import { useState, useEffect } from "react";
+
 function App() {
-  return <h1>Ambient_player start!</h1>;
+  useEffect(() => {
+    const cards = document.querySelectorAll(".card");
+
+    function removeActiveClasses() {
+      cards.forEach((card) => {
+        card.classList.remove("active");
+      });
+    }
+
+    cards.forEach((card) => {
+      card.addEventListener("click", () => {
+        removeActiveClasses();
+
+        const isActive = card.classList.contains("active");
+
+        if (!isActive) {
+          card.classList.add("active");
+        }
+      });
+    });
+  });
+
+  return (
+    <div className="container">
+      <div className="card active">
+        <h3>바람 소리</h3>
+      </div>
+      <div className="card">
+        <h3>빗 소리</h3>
+      </div>
+      <div className="card">
+        <h3>천둥 소리</h3>
+      </div>
+      <div className="card">
+        <h3>빗물 소리</h3>
+      </div>
+      <div className="card">
+        <h3>바다 소리</h3>
+      </div>
+    </div>
+  );
 }
 
 export default App;
