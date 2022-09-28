496769bc26eafa919a108b017799a22908172e65
leejanghe <babybye777@gmail.com>
2022-09-29 02:48:43 +0900

    애니메이션 & mp3파일 업로드 및 css 효과 추가

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,22 @@
 import "./App.css";
 import { useState, useEffect, useRef } from "react";
 import Lottie from "lottie-react-web";
-import boom from "../src/animation/boom.json";
+
+import red from "./animation/red.json";
+import orange from "./animation/orange.json";
+import yellow from "./animation/yellow.json";
+import green from "./animation/green.json";
+import blue from "./animation/blue.json";
+import indigo from "./animation/indigo.json";
+import purple from "./animation/purple.json";
+
 import redSound from "./mp3/red.mp3";
+import orangeSound from "./mp3/orange.mp3";
+import yellowSound from "./mp3/yellow.mp3";
+import greenSound from "./mp3/green.mp3";
+import blueSound from "./mp3/blue.mp3";
+import indigoSound from "./mp3/indigo.mp3";
+import purpleSound from "./mp3/purple.mp3";
 
 function App() {
   useEffect(() => {
@@ -11,24 +25,22 @@ function App() {
     function removeActiveClasses() {
       cards.forEach((card) => {
         card.classList.remove("active");
-        // handlePause();
       });
     }
 
     cards.forEach((card) => {
       card.addEventListener("click", () => {
         removeActiveClasses();
-        handlePause();
         const isActive = card.classList.contains("active");
 
         if (!isActive) {
           card.classList.add("active");
-          handlePlay();
         }
       });
     });
   });
 
+  // 소리 제어
   const audioRef = useRef(null);
 
   const handlePlay = () => {
@@ -42,52 +54,75 @@ function App() {
   return (
     <div className="container">
       <div className="card active">
-        <Lottie options={{ animationData: boom, loop: true, autoplay: true }} />
+        <div className="card__inner">
+          <Lottie
+            options={{ animationData: red, loop: true, autoplay: true }}
+          />
+        </div>
         <audio
-          src={redSound}
+          src={purpleSound}
           autoPlay="autoPlay"
           type="audio/mp3"
           ref={audioRef}
         />
-        <h3>빨간 소리</h3>
       </div>
+
       <p onClick={handlePlay}>재생</p>
       <p onClick={handlePause}>정지</p>
       <div className="card">
-        <Lottie options={{ animationData: boom, loop: true, autoplay: true }} />
-        <h3>주황 소리</h3>
+        <div className="card__inner">
+          <Lottie
+            options={{ animationData: orange, loop: true, autoplay: true }}
+          />
+        </div>
+        {/* <audio
+          src={orangeSound}
+          autoPlay="autoPlay"
+          type="audio/mp3"
+          ref={audioRef}
+        /> */}
       </div>
 
       <div className="card">
-        <Lottie options={{ animationData: boom, loop: true, autoplay: true }} />
-        <h3>노랑 소리</h3>
+        <div className="card__inner">
+          <Lottie
+            options={{ animationData: yellow, loop: true, autoplay: true }}
+          />
+        </div>
       </div>
 
       <div className="card">
-        <Lottie options={{ animationData: boom, loop: true, autoplay: true }} />
-        <h3>초록 소리</h3>
+        <div className="card__inner">
+          <Lottie
+            options={{ animationData: green, loop: true, autoplay: true }}
+          />
+        </div>
       </div>
 
       <div className="card">
-        <Lottie options={{ animationData: boom, loop: true, autoplay: true }} />
-        <h3>파랑 소리</h3>
+        <div className="card__inner">
+          <Lottie
+            options={{ animationData: blue, loop: true, autoplay: true }}
+          />
+        </div>
       </div>
 
       <div className="card">
-        <Lottie options={{ animationData: boom, loop: true, autoplay: true }} />
-        <h3>남색 소리</h3>
+        <div className="card__inner">
+          <Lottie
+            options={{ animationData: indigo, loop: true, autoplay: true }}
+          />
+        </div>
       </div>
 
       <div className="card">
-        <Lottie options={{ animationData: boom, loop: true, autoplay: true }} />
-        <h3>보라 소리</h3>
+        <div className="card__inner">
+          <Lottie
+            options={{ animationData: purple, loop: true, autoplay: true }}
+          />
+        </div>
       </div>
     </div>
-    // <Lottie
-    //   options={{ animationData: boom, loop: true, autoplay: true }}
-    //   height={400}
-    //   width={400}
-    // />
   );
 }
 
