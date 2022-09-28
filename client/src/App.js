624b66ff5cbdce495f5001153998fcd226f61291
leejanghe <babybye777@gmail.com>
2022-09-29 00:28:12 +0900

    mp3 오디오 태스트

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,8 @@
 import "./App.css";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import Lottie from "lottie-react-web";
 import boom from "../src/animation/boom.json";
+import redSound from "./mp3/red.mp3";
 
 function App() {
   useEffect(() => {
@@ -10,29 +11,48 @@ function App() {
     function removeActiveClasses() {
       cards.forEach((card) => {
         card.classList.remove("active");
+        // handlePause();
       });
     }
 
     cards.forEach((card) => {
       card.addEventListener("click", () => {
         removeActiveClasses();
-
+        handlePause();
         const isActive = card.classList.contains("active");
 
         if (!isActive) {
           card.classList.add("active");
+          handlePlay();
         }
       });
     });
   });
 
+  const audioRef = useRef(null);
+
+  const handlePlay = () => {
+    audioRef.current.play();
+  };
+
+  const handlePause = () => {
+    audioRef.current.pause();
+  };
+
   return (
     <div className="container">
       <div className="card active">
         <Lottie options={{ animationData: boom, loop: true, autoplay: true }} />
+        <audio
+          src={redSound}
+          autoPlay="autoPlay"
+          type="audio/mp3"
+          ref={audioRef}
+        />
         <h3>빨간 소리</h3>
       </div>
-
+      <p onClick={handlePlay}>재생</p>
+      <p onClick={handlePause}>정지</p>
       <div className="card">
         <Lottie options={{ animationData: boom, loop: true, autoplay: true }} />
         <h3>주황 소리</h3>
